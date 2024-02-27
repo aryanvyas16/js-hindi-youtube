d53ea7d91dd28e6d569e00cdd7f18042dd147fd2
Aryan Vyas <113963972+aryanvyas16@users.noreply.github.com>
2024-02-27 06:59:37 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+let UserName= "Aryan Vyas";
+let pass= "DaalChawal";
+let age= 20;
+let isLoggedIn=false;
+let temp=null;
+let state;
+
+console.table([UserName,pass,age,isLoggedIn,temp,state])
+
+console.log(typeof temp)
+
+//Datatypes:-
+//strings-> " "
+//undefined->
+//null-> 
+//bool-> true/false
+//number-> 2 to power53
+//bigint
+//symbol->unique
+
+//object
+//null type is object
+//jhgf
