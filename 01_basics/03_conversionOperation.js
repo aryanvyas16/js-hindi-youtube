9213186edd2fafcbc4d9ee66141125afd6eeaf0f
Aryan Vyas <113963972+aryanvyas16@users.noreply.github.com>
2024-02-28 05:45:14 +0000

    conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,9 +27,47 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber=33;
 let StringSomeNumber=String(someNumber);
-console.log(StringSomeNumber);
-console.log(typeof StringSomeNumber);
+// console.log(StringSomeNumber);
+// console.log(typeof StringSomeNumber);
 
 //so converted to string
+
+
+//***********************************************operationss*******************************************************
+
+
+let value=3;
+let negValue=-3;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);// 2 ki power
+// console.log(2/2);
+// console.log(2%3);// remainder
+
+let str1 = "Hello"
+let str2 = " Aryan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);  //agr string first hai toh sabhi ko string ki tarah treat kiya jayega
+// console.log(1 + 2 + "2");   // agr int first hai hai toh pehle int operation karenge uske baad string likhenge
+// console.log((3+4)*4/5);  //using bracet is a good practise
+
+// console.log(+true);
+// console.log(+"");
+
+let num1 ,num2, num3
+num1 , num2 , num3 =2+2;
+
+let gameCounter=100;
+// gameCounter++;  //postfix ->isme pehle value assign hoti hai uske baad increment hota hai
+++gameCounter;      //prefix ->isme pehle increment hota hai uske baad value assign hoti hai
+console.log(gameCounter);
+
 
 
